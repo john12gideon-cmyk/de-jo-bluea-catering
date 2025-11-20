79bd2e9988401d06ef9c997420d58940e28af335
john12gideon-cmyk <john12gideon@gmail.com>
2025-11-20 10:34:03 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase modules
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBGasIhrjc-7nv4JlYct0on9Hk-2JMD76c",
-  authDomain: "de-je-blue.firebaseapp.com",
-  projectId: "de-je-blue",
-  storageBucket: "de-je-blue.firebasestorage.app",
+  apiKey: "AIzaSyB6asthrjc-7nv4JlyCt0on9HK-2JMD76C",
+  authDomain: "de-jo-bluea.firebaseapp.com",
+  projectId: "de-jo-bluea",
+  storageBucket: "de-jo-bluea.appspot.com",
   messagingSenderId: "213345773472",
   appId: "1:213345773472:web:5d0fbed7b68741e2c2e6c5",
   measurementId: "G-V187CTTKMT"
@@ -18,4 +16,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
+
+// Export Firestore instance
+export { db };
